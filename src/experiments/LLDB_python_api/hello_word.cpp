a597f1ac8ecac7bea9a8b8c2cef05f2fb535d017
BobHell0 <anujan.cenan@gmail.com>
2025-10-12 18:03:46 +1100

    transcribed the debug2.py program in c++ - needed to install the LLVM library - compilation command to be included

--- src/experiments/LLDB_python_api/hello_word.cpp
+++ src/experiments/LLDB_python_api/hello_word.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-
+int f()
+{
+    int a = 1;
+    return 6;
+}
 int main()
 {
+
     int x = 3;
     int y = 4;
     int z =  x * y;
+
+    int g = f();
+
+    int t = f() + 3 - 6;
 
     std::cout << x << y << z << '\n';
     
