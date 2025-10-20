51c7ac2ac076d54a725aa971bf2e21cb7f758220
BobHell0 <anujan.cenan@gmail.com>
2025-10-20 21:02:46 +1100

    added a directory selection button - for setting current working directory

--- src/tkinter_frontend/main.py
+++ src/tkinter_frontend/main.py
@@ -1,5 +1,7 @@
+import os
 import tkinter as tk        # normal
 import tkinter.ttk as ttk   # themed
+from tkinter import filedialog
 
 from constants import *
 from Memory_Object import Memory_Object
@@ -8,8 +10,24 @@ from Grid_Drawing import Grid_Drawing
 from LLDB_Interaction import *
 
 import posix_ipc
+from Generate_Message import *
 from multiprocessing import shared_memory
 
+
+sem = posix_ipc.Semaphore(SEMAPHORE_NAME)
+shm = shared_memory.SharedMemory(SHARED_MEMORY_REGION_NAME, create=False)     # default to false
+
+def send_message(message: str):
+    sem.acquire()
+    encoded = message.encode('utf-8')
+    shm.buf[:len(encoded)] = encoded
+    sem.release()
+
+def read_message():
+    sem.acquire()
+    data = shm.buf[:MAX_MESSAGE_SIZE].tobytes().decode()
+    sem.release()
+    return data
 
 
 window = tk.Tk()
@@ -37,6 +55,22 @@ mem_object = Memory_Object("x", "int", 3, 4, 42343123, 5)
 next_memory = 92
 next_memory = gd.add_memory_object(mem_object, next_memory, "red")
 
+
+#### File selection - for selecting the current working directory
+def select_file():
+    print(f"PWD = {os.getcwd()}")       # gets cwd from **where** the python program was launched from
+    dir_path = filedialog.askdirectory(initialdir=os.getcwd())
+    print(f"File path = {dir_path}")
+    message  = set_project_dir_message(dir_path)
+    print(f"Going to send message: {message}")
+    send_message(message)
+    return dir_path
+
+
+open_button = tk.Button(window, text="Open File", command=select_file)
+open_button.pack(pady=20)
+
+
 ### Event Handling
 
 def arrow_right(_: tk.Event):
@@ -46,11 +80,9 @@ window.bind("<Right>", arrow_right)
 
 window.mainloop()
 
-sem = posix_ipc.Semaphore(SEMAPHORE_NAME)
-shm = shared_memory.SharedMemory(SHARED_MEMORY_REGION_NAME, create=False)
 
 while (True):
-    sem.acquire()
+    # sem.acquire()
 
     next_event = 0
 
@@ -65,7 +97,7 @@ while (True):
             pass
         
 
-    sem.release()
+    # sem.release()
     
     input_mes = "Q"
     if (input_mes == "Q"): break
